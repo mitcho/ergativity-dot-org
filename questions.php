618c041dc9e6139170a5daf4fd9593085c2c8708
Michael Yoshitaka Erlewine <mitcho@mitcho.com>
2014-11-26 02:04:38 -0500

    + answer shortcode

--- questions.php
+++ questions.php
@@ -49,11 +49,15 @@ class ErgAnswer {
 		echo "<td><input type='text' size='30' name='erg_answers[{$erg_meta_print_key}][label]' value='" . esc_attr($this->label) . "'/></td>";
 		echo "</tr>";
 	}
+
+	function visitor_print( $post_id ) {
+		return "<p><input type='radio' name='erg_answers[{$post_id}]' id='erg_answers-{$this->value}' value='{$this->value}' " . disabled( !is_user_logged_in(), true, false ) . "/> <label for='erg_answers-{$this->value}'>" . esc_html($this->label) . "</label></p>";
+	}
 }
 
 add_action('add_meta_boxes', 'erg_meta_boxes');
 function erg_meta_boxes() {
-	add_meta_box( 'erg_answers', 'Answers', 'erg_answers_metabox', 'question' );
+	add_meta_box( 'erg_answers', 'Answers', 'erg_answers_metabox', 'question', 'normal' );
 }
 function erg_answers_metabox( $post ) {
 	wp_nonce_field( 'erg_answers_metabox', 'erg_answers_metabox_nonce' );
@@ -114,6 +118,32 @@ function erg_answers_save( $post_id ) {
 
 	// Update the meta field in the database.
 	add_post_meta( $post_id, '_erg_answers', $answers, true );
+}
+
+add_shortcode( 'answer', 'erg_answer_shortcode' );
+function erg_answer_shortcode( $atts ) {
+	$answers = get_post_meta( get_the_ID(), '_erg_answers', true );
+	
+	if ( empty($answers) || !is_array($answers) )
+		return;
+	
+	$return = '';
+	if ( !is_user_logged_in() ) {
+		$return .= "<div class='warning'>";
+		$return .= "<p>By logging in to ergativity.org, you will be able to save your answers to individual questions and the questionnaire will guide you through relevant questions.</p>";
+			$return .= "<p><a href='" . esc_url(wp_login_url()) . "'>Log in</a> or <a href='" . esc_url(site_url( 'wp-login.php?action=register', 'login' )) . "'>create a new account</a></p>";
+		// @todo make these urls come back to the right page
+		$return .= "</div>";
+	}
+	
+	$return .= "<div class='answer'>";
+
+	foreach ($answers as $answer) {
+		$return .= (new ErgAnswer($answer))->visitor_print( get_the_ID() );
+	}
+	
+	$return .= "</div>";
+    return $return;
 }
 
 add_shortcode( 'your language', 'erg_lang_shortcode' );
